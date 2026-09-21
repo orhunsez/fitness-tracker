d645efee12e8b269705c1de785d692d99dd6b115
orhunsez <orhunsez@gmail.com>
2026-09-22 01:25:35 +0300

    fix : dark mode implemented && feat : login and signup, theme manager

--- client/src/context/AppContext.tsx
+++ client/src/context/AppContext.tsx
@@ -1,4 +1,45 @@
 
-import { createContext } from "react";
-import { initialState } from "../types";
+import React, { createContext, useContext, useState } from "react";
+import { initialState, type ActivityEntry, type Credentials, type FoodEntry } from "../types";
+import { useNavigate } from "react-router-dom";
+import mockApi from "../assets/mockApi";
 
+const AppContext = createContext(initialState)
+
+export const AppProvider = ({children} : {children : React.ReactNode})=> {
+
+    const navigate = useNavigate()
+    const [user, setUser] = useState<User>(null)
+    const [isUserFetched, setIsUserFetched] = useState(false)
+    const [onboardingCompleted, setOnboardingCompleted] = useState(false)
+    const [allFoodLogs, setAllFoodLogs] = useState<FoodEntry>([])
+    const [allActivityLogs, setAllActivityLogs] = useState<ActivityEntry>([])
+
+    // Signup Function
+    const signup = async (credentials : Credentials)=>{
+        const {data}  = await mockApi.auth.register(credentials)
+        setUser(data.user)
+        if (data?.user?.age && data?.user?.weight && data?.user?.goal) {
+            setOnboardingCompleted(true)
+        }
+        localStorage.setItem('token', data.jwt)
+    }
+
+    //Login Function
+    const login = async (credentials : Credentials)=>{
+        const {data} = await mockApi.auth.login(credentials)
+        setUser({...data.user, token: data.jwt})
+        if (data?.user?.age && data?.user?.weight && data?.user?.goal) {
+            setOnboardingCompleted(true)
+        }
+        localStorage.setItem('token', data.jwt)
+    }
+
+    const value = {}
+
+    return <AppContext.Provider value={value}>
+        {children}
+    </AppContext.Provider>
+}
+
+export const useAppContext = ()=> useContext(AppContext)
